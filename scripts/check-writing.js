a8939fada38a2b4b7ef9832b016b1937b8614c20
Evan Klemen <ejklemen@gmail.com>
2015-01-22 16:46:04 -0500

    @eklemen @carolinapowers

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -10,17 +10,31 @@ it('should return 1 as string "one"', function(){
 })
 */
 
+//function toEnglish (a){
+//    if(a == 2) {
+//        return "two";    
+//    } else {
+//        return "this is not the number 2";
+//    }
+//    
+//}
+
+//console.log(toEnglish(3));
+//
+//it('should return "two" when 2 is entered and "not two" if any other number is entered', function(){
+//    assert.equal(toEnglish(2), "two");
+//})
+
 function toEnglish (a){
-    if(a == 2) {
-        return "two";    
+    if (a == 3){
+        return "three";
     } else {
-        return "this is not the number 2";
+        return "this is not the number three"
     }
-    
 }
 
 console.log(toEnglish(3));
 
-it('should return "two" when 2 is entered and "not two" if any other number is entered', function(){
-    assert.equal(toEnglish(2), "two")
+it('should turn 3 into "three", else its not 3', function (){
+    assert.equal(toEnglish(3), "three");
 })
