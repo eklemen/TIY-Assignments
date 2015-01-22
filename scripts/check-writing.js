4a2c4f4b4ae7b64ef30af9666c5521c8c67481ea
Evan Klemen <ejklemen@gmail.com>
2015-01-22 16:16:04 -0500

    @eklemen @carolina powers

---
+++ scripts/check-writing.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+
+function toEnglish(a){
+    return "one";
+    
+}
+
+it('should return 1 as string "one"', function(){
+    assert.equal(toEnglish(1), "one");
+})
+
