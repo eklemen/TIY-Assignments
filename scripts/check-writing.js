ab43f2dff2b2db1b68f8de837ae0a277f45e2128
Evan Klemen <ejklemen@gmail.com>
2015-01-22 16:24:29 -0500

    @eklemen @carolinapowers

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 
-function toEnglish(a){
+/*function toEnglish(a){
     return "one";
     
 }
@@ -8,4 +8,19 @@ function toEnglish(a){
 it('should return 1 as string "one"', function(){
     assert.equal(toEnglish(1), "one");
 })
+*/
 
+function toEnglish (a){
+    if(a == 2) {
+        return "two";    
+    } else {
+        return "this is not the number 2";
+    }
+    
+}
+
+console.log(toEnglish(3));
+
+it('should return "two" when 2 is entered and "not two" if any other number is entered', function(){
+    assert.equal(toEnglish(2), "two")
+})
