18ded77ccb2ccbacbc96c1e4969fbfa4022547de
Evan Klemen <ejklemen@gmail.com>
2015-01-26 17:16:48 -0500

    Removed the else if's and added in a sleek function with some tests

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -1,37 +1,47 @@
 var assert = require('assert');
-
+console.log(Date());
 
 var toEnglish = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 
-var whatNum = function(a){
-    if(a == 0){
-        return toEnglish[0];
-    } else if(a == 1){
-        return toEnglish[1];
-    } else if(a == 2){
-        return toEnglish[2];
-    } else if(a == 3) {
-        return toEnglish[3];
-    } else if(a == 4){
-        return toEnglish[4];
-    } else if(a == 5){
-        return toEnglish[5];
-    } else if(a == 6){
-        return toEnglish[6];
-    } else if(a == 7){
-        return toEnglish[7];
-    } else if(a == 8){
-        return toEnglish[8];
-    } else if(a == 9){
-        return toEnglish[9];
-    } else if(a == 10){
-        return toEnglish[10];
-    } else {
-        console.log("i can't count that high yet");
-    }
+function printer(n){
+    console.log(toEnglish[n]);
+    return toEnglish[n];
 }
 
-console.log(whatNum(3));
+it('should print the string number value for the integer given to printer', function(){
+    assert.equal(printer(5), "five");
+    assert.equal(printer(4), "five");
+    });
+
+//var whatNum = function(a){
+//    if(a == 0){
+//        return toEnglish[0];
+//    } else if(a == 1){
+//        return toEnglish[1];
+//    } else if(a == 2){
+//        return toEnglish[2];
+//    } else if(a == 3) {
+//        return toEnglish[3];
+//    } else if(a == 4){
+//        return toEnglish[4];
+//    } else if(a == 5){
+//        return toEnglish[5];
+//    } else if(a == 6){
+//        return toEnglish[6];
+//    } else if(a == 7){
+//        return toEnglish[7];
+//    } else if(a == 8){
+//        return toEnglish[8];
+//    } else if(a == 9){
+//        return toEnglish[9];
+//    } else if(a == 10){
+//        return toEnglish[10];
+//    } else {
+//        console.log("i can't count that high yet");
+//    }
+//}
+//
+//console.log(whatNum(3));
 
 //it('should print the string number of what is entered 0-5', function(){
 //    assert
