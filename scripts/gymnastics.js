10f5a863c6081dfb72ca6de698bc15dc7201b03a
Evan Klemen <ejklemen@gmail.com>
2015-01-27 14:09:32 -0500

    Followed along with api gymnastics

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -4,32 +4,86 @@ console.log(events.length); //prints 30, I'm assuming 30 events but that doesnt 
 
 assert(events.length);
 
-function answer () {
+function answer(){
+    var index = 0;
+    var numberOfPushEvents = 0; 
+    // can leave off index and array if not using them
+ 
+    events.forEach(function(item, index, array){
+        if(item.type == "PushEvent"){
+            numberOfPushEvents++;
+        }
+    });
+    
+    var pushEvents = events.filter(function(item){
+        return item.type == "PushEvent";
+        
+    });
+    pushEvents.length == numberOfPushEvents;
+    
     return {
-        'total': events.length
+        'total': events.length,
+        'PushEvent': {
+            'total': numberOfPushEvents,
+        }
     };
-} // end answer
+}
 
-describe('the setup', function (){
+describe('Should determine that events and answer exist, also what type they are', function(){
+it('should be test for events and answer', function(){
+    assert(events);
+    assert(events instanceof Array);
+    assert(answer);
+    assert(typeof answer == 'function');
+})
+})
 
-    it('should have events', function (){
-        assert(events);
-    });
-
-    it('should have an "answer" function', function(){
-        assert(answer);
-        assert(typeof answer == 'function');
-    });
-});
-describe('the answer', function (){
-    var theAnswer = answer ();
+describe("tests for key values", function(){
+    it('should have 30 total events', function(){
+        assert(events.length > 0)
+        assert.equal(events.length, 30)
+    })  
     
-    it('should have 30 total events', function (){
-        assert(theAnswer.total === 30);
-    });
+})
+
     
-    it('should have some `PushEvent` entries', function(){
-        assert(theAnswer.PushEvents);
-        assert(theAnswer.PushEvents.total);
-    });
-});
+    
+//    while (index < events.length) {
+//        var event = events[index];
+//        
+//        if (event.type == "PushEvent"){
+//            numberOfPushEvents++;
+//        }
+//        
+//        index++;
+//    }
+//    
+//    
+//    return {
+//        'total': events.length
+//    };
+// end answer
+//
+//describe('the setup', function (){
+//
+//    it('should have events', function (){
+//        assert(events);
+//    });
+//
+//    it('should have an "answer" function', function(){
+//        assert(answer);
+//        assert(typeof answer == 'function');
+//    });
+//});
+//describe('the answer', function (){
+//    var theAnswer = answer ();
+//    
+//    it('should have 30 total events', function (){
+//        assert(theAnswer.total === 30);
+//    });
+//    
+//    it('should have some `PushEvent` entries', function(){
+//        assert(theAnswer.PushEvents);
+//        assert(theAnswer.PushEvents.total);
+//    });
+//});
