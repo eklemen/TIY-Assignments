4e9f618c36d2198e953ae53d4004c47df6eff40f
Evan Klemen <ejklemen@gmail.com>
2015-01-27 09:49:41 -0500

    Tests on gymnastics.js

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -1,3 +1,35 @@
 var events = require('../apis/github/users/eklemen/events.json');
-
+var assert = require('assert');
 console.log(events.length); //prints 30, I'm assuming 30 events but that doesnt seem right hmm...
+
+assert(events.length);
+
+function answer () {
+    return {
+        'total': events.length
+    }
+} // end answer
+
+describe('the setup', function (){
+
+    it('should have events', function (){
+        assert(events);
+    });
+
+    it('should have an "answer" function', function(){
+        assert(answer);
+        assert(typeof answer == 'function');
+    });
+});
+describe('the answer', function (){
+    var theAnswer = answer ();
+    
+    it('should have 30 total events', function (){
+        assert(theAnswer.total === 30);
+    });
+    
+    it('should have some `PushEvent` entries', function(){
+        assert(theAnswer.PushEvents);
+        assert(theAnswer.PushEvents.total);
+    });
+});
