0c269401d771f3d489e501633fb43840680fe1a3
Evan Klemen <ejklemen@gmail.com>
2015-01-28 18:02:19 -0500

    end of the 3s+5s

--- scripts/3s+5s.js
+++ scripts/3s+5s.js
@@ -1,30 +1,17 @@
 var assert = require('assert');
 
 
-function solution () {
-}
+function solution (x) {
+    var sums = 0;
 
-
-var passingNums = [];
-
-for (var i = 1; i < 10; i++){
-    if (i % 3 == 0){
-        console.log(i);
+    for (var i = 1; i < x; i++){
+        if (i % 3 === 0 || i % 5 === 0){
+            sums+=i;
+        }
+        
     }
+    return sums;
 }
-
-console.log(passingNums);
-//for (var b = 1; b < 10; b++){
-//    if (b % 3 == 0){
-//        console.log(b);
-//    }
-//}
-//
-//        }
-//    if (i % 5 == 0){
-//        console.log(i);
-//    }
-
 console.log(Date());
 
 it('should have a `solution` function', function(){
@@ -34,16 +21,20 @@ it('should have a `solution` function', function(){
 
 it('should have a `solution` for (10, 20, 30)', function(){
   assert.equal(solution(10), 23);
-  assert.equal(solution(20), undefined);
-  assert.euqal(solution(30), undefined);
+  assert.equal(solution(20), 78);
+  assert.equal(solution(30), 195);
 });
 
 it('should have a `solution` for 50', function(){
-  assert.equal(solution(50), undefined);
+  assert.equal(solution(50), 543);
 });
 
 it('should have a solution for 100', function(){
-  assert.equal(solution(100), undefined);
+  assert.equal(solution(100), 2318);
+});
+
+it('should have a solution for 1000', function(){
+  assert.equal(solution(1000), 233168);
 });
 
 // What about 1000?
