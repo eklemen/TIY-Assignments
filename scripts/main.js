0167b9aeb1e4d59a55c03eea787c9f83a78c89d3
Evan Klemen <ejklemen@gmail.com>
2015-01-28 20:15:26 -0500

    Added a few documentations

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,83 @@
-var board = [
-['R','N','B','Q','K','B','N','R'],
-['P','P','P','P','P','P','P','P'],
-[' ',' ',' ',' ',' ',' ',' ',' '],
-[' ',' ',' ',' ',' ',' ',' ',' '],
-[' ',' ',' ',' ',' ',' ',' ',' '],
-[' ',' ',' ',' ',' ',' ',' ',' '],
-['p','p','p','p','p','p','p','p'],
-['r','n','b','q','k','b','n','r'],
-];
+var _ = require('lodash')
+function print(board){
+    
+    console.log('<tabel class="chessboard">');
+    
+    _.forEach(board, function(row, rank){
+        var array = new Array(8);
+        for (var i = 0; i < 8; i++){
+            array[i] = [' '];
+        }
+      // Before any cells are printed...
 
-var printBoard = function() {console.log(board.join('\n') + '\n\n');}
+      _.forEach(row, function(square, file){
 
-function move (toY, toX, fromY, fromX){
-    toX = toX -1;
-    toY = toY - 1;
-    fromX = fromX - 1;
-    fromY = fromY -1;
-    board[toY][toX] = board[fromY][fromX];
-    board[fromY][fromX] = ' ';
-    printBoard();
-}
+          // In here, we have `rank`, `file`, and `square`...
 
-// toY X frmY X
-move(5, 5, 7, 5);
-move(4, 4, 2, 4);
-move(4, 4, 5, 5);
-move(3, 8, 2, 8);
-move(6, 2, 7, 2);
+      }); // END foreach(row)
+
+      // After all the cells are printed...
+
+  }); // END foreach(board)
+
+  // After all the rows are printed...
+  console.log('</table> <!-- .chessboard -->');
+} // END print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var board = [
+//['R','N','B','Q','K','B','N','R'],
+//['P','P','P','P','P','P','P','P'],
+//[' ',' ',' ',' ',' ',' ',' ',' '],
+//[' ',' ',' ',' ',' ',' ',' ',' '],
+//[' ',' ',' ',' ',' ',' ',' ',' '],
+//[' ',' ',' ',' ',' ',' ',' ',' '],
+//['p','p','p','p','p','p','p','p'],
+//['r','n','b','q','k','b','n','r'],
+//];
+//
+//var printBoard = function() {console.log(board.join('\n') + '\n\n');}
+//
+//function move (toY, toX, fromY, fromX){
+//    toX = toX -1;
+//    toY = toY - 1;
+//    fromX = fromX - 1;
+//    fromY = fromY -1;
+//    board[toY][toX] = board[fromY][fromX];
+//    board[fromY][fromX] = ' ';
+//    printBoard();
+//}
+//
+//// toY X frmY X
+//move(5, 5, 7, 5);
+//move(4, 4, 2, 4);
+//move(4, 4, 5, 5);
+//move(3, 8, 2, 8);
+//move(6, 2, 7, 2);
 
 // This is the code to manually move each piece, which the code block of the function move was based on.
 
