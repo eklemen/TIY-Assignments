6c18142cfc01a90d9484e75d5d7d43cf8923fba0
Evan Klemen <ejklemen@gmail.com>
2015-02-02 16:57:46 -0500

    Notes from mon feb 2

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,9 @@
+var clickMe = function(){
+    this.nextElementSibling.className = "open";
+}
 
+var h2s = document.getElementsByTagName('h2');
+
+for(var index=0; index< h2s.length; index++){
+    h2s[index].onclick = clickMe;
+}
