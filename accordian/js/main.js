d5eb221455cfd255baea697aeab1f59b6d29b1d8
Evan Klemen <ejklemen@gmail.com>
2015-02-02 21:34:13 -0500

    Shows p inside with the other anchor links, havent commited in a while thought i should

--- accordian/js/main.js
+++ accordian/js/main.js
@@ -1,1 +1,20 @@
+var clickMe = function(){
+    this.nextElementSibling.className = "open";
+//    if (this.nextElementSibling.style.display == "none"){
+//        this.nextElementSibling.className = "open";
+//    } else {
+//        this.nextElementSibling.style.display = "none";
+//    }
+        
+}
 
+var h2s = document.getElementsByTagName('h2');
+var h3s = document.getElementsByTagName('h3');
+
+for(var index=0; index< h2s.length; index++){
+    h2s[index].onclick = clickMe;
+}
+
+for(var index=0; index< h3s.length; index++){
+    h3s[index].onclick = clickMe;
+}
