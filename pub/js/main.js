adcc88c5ab196d9e2691c1e82a4d9e163a84996d
Evan Klemen <ejklemen@gmail.com>
2015-02-07 18:37:59 -0500

    Pulled single datas from the curledJSON, then pulled data from api.url for the popular repo section with a .map to iterate over the repos that i have

--- pub/js/main.js
+++ pub/js/main.js
@@ -7,7 +7,26 @@
         var path = '../apis/github/users/eklemen/eklemen.json'
         
         $.getJSON(path, function(json){
-            $('.contact a').html(json.login);
+            var user = json;
+            $('#location').html(user.location);
+            $('#email').html(user.email)
+                .attr('href', 'mailto:' + user.email);
+            $('#github-link').html(user.html_url)
+                .attr('href', user.html_url);
+            $('#followers').html(user.followers + "Followers");
+            $('#following').html(user.following + "Following");
+          
+//            Popular Repo Info
+            $.getJSON(user.repos_url, function(repos){
+
+           var repoItems = $.map(repos, function(name, i){
+                  var listItem = $('<li></li>')
+                  $('<h3>' + repos[i].name + '</h3>').appendTo(listItem);
+//               console.log(repos[i].name);     
+               return listItem;
+              });
+              $('.repo-list').html(repoItems);
+          })
         });
         
             });
