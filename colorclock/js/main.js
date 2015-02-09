80b299f4da78aabc490d4d49b9dc84e98a138ad8
Evan Klemen <ejklemen@gmail.com>
2015-02-09 18:17:18 -0500

    didnt commit enough, got the clock to read as hexdecimal and change the body background color, not very bright colors however

--- colorclock/js/main.js
+++ colorclock/js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
 
+    var jsTime = function() {
+        var t = new Date();
+        var timeOnly = t.toTimeString().slice(0, 8);
+        var hours = timeOnly.slice(0,2);
+        var min = timeOnly.slice(3, 5);
+        var sec = timeOnly.slice(6, 8)
+        var color = '#'+hours+min+sec;
+        var change = $('body').css("background-color", color);
+        $('p').html(hours+ ' ' + min + ' ' +sec);
+    };
+    setInterval(function(){jsTime()}, 1000);
+    
+})
