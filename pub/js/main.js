a11fde69e03fe7c07f35804ac857e377d2c5c921
Evan Klemen <ejklemen@gmail.com>
2015-02-10 16:34:36 -0500

    Seeing what .prototype does

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
     var cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
-    function Board(){
-        
-    }
+//    function Board(){
+//        document.write('<table id="chessboard">');
+//        $('#chessboard').appendTo('tr');
+//            
+//        
+//        document.write('</table>');
+//    }
     
     function Piece(color, name, position){
        this._color = color;
        this._name = name;
        this._position = position;
-   }
+       }
     
-    var whiteQueen = new Piece('white', 'queen', "D1");
-    Piece.prototype = {
-    }; 
+    var whiteQueen = new Piece('white', 'queen', 'D1');
+    var whiteKing = new Piece('white', 'king', 'E1');
+    
+    
+    Piece.prototype.type = 'piece';
+    
+    
     console.log(whiteQueen);
     
 });
