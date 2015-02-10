f2f41e1fa41a6dde26338d2213ae061b885c72f6
Evan Klemen <ejklemen@gmail.com>
2015-02-09 22:33:39 -0500

    tried converting to hex, but was left with single digits displaying on my clock

--- colorclock/js/main.js
+++ colorclock/js/main.js
@@ -2,19 +2,21 @@ $(document).ready(function(){
 
     var jsTime = function() {
         var t = new Date();
-//        var timeOnly = t.toTimeString().slice(0, 8);
-//        var hours = timeOnly.slice(0,2);
-//        var min = timeOnly.slice(3, 5);
-//        var sec = timeOnly.slice(6, 8);
-        var hours = t.getHours();
-        var min = t.getMinutes();
-        var sec = t.getSeconds();
-        var color = '#' + hours.toString(16) + min.toString(16); + sec.toString(16);
+        var timeOnly = t.toTimeString().slice(0, 8);
+        var hours = timeOnly.slice(0,2);
+        var min = timeOnly.slice(3, 5);
+        var sec = timeOnly.slice(6, 8);
+//        var hours = t.getHours();
+//        var hourstr = hours.toString();
+//        var min = t.getMinutes();
+//        var minstr = min.toString();
+//        var sec = t.getSeconds();
+//        var secstr = sec.toString();
+        var color = '#' + hours + min + sec;
         var change = $('body').css("backgroundColor", color);
         $('#clock > p').html(hours + ':' + min + ':' + sec);
+        console.log(sec.toString(16));
     };
     setInterval(function(){jsTime()}, 1000);
-    var nums = new Date();
-    console.log(nums.toString(16));
     
 })
