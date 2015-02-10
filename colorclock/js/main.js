0cb9cf3ec5388fb4fe4d12ec15b385d2517604b8
Evan Klemen <ejklemen@gmail.com>
2015-02-09 22:04:18 -0500

    Trying to use .getHours instead of slice to get the value, not changing color now tho

--- colorclock/js/main.js
+++ colorclock/js/main.js
@@ -2,16 +2,19 @@ $(document).ready(function(){
 
     var jsTime = function() {
         var t = new Date();
-        var timeOnly = t.toTimeString().slice(0, 8);
-        var hours = timeOnly.slice(0,2);
-        var min = timeOnly.slice(3, 5);
-        var sec = timeOnly.slice(6, 8);
-        var color = '#'+hours+min+sec;
-        var change = $('body').css("background-color", color);
+//        var timeOnly = t.toTimeString().slice(0, 8);
+//        var hours = timeOnly.slice(0,2);
+//        var min = timeOnly.slice(3, 5);
+//        var sec = timeOnly.slice(6, 8);
+        var hours = t.getHours();
+        var min = t.getMinutes();
+        var sec = t.getSeconds();
+        var color = '#' + hours.toString(16) + min.toString(16); + sec.toString(16);
+        var change = $('body').css("backgroundColor", color);
         $('#clock > p').html(hours + ':' + min + ':' + sec);
     };
     setInterval(function(){jsTime()}, 1000);
-    var nums = 14;
+    var nums = new Date();
     console.log(nums.toString(16));
     
 })
