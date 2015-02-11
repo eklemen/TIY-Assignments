e7c56e018dcb9eafb432e17ccd73c631b1caa67a
Evan Klemen <ejklemen@gmail.com>
2015-02-11 14:37:29 -0500

    changed back to manual linking cause wiredeps was weird

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,24 +1,31 @@
+// var assert = require('chai').assert;
+var vm = new Vue({ /* options */ });
+//
+// assert.isDefined(vm.$el);
+// console.log(vm.$data);
+// console.assert(vm.$data);
+
 var assert = require('assert');
 
     var cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
 //    function Board(){
 //        document.write('<table id="chessboard">');
 //        $('#chessboard').appendTo('tr');
-//            
-//        
+//
+//
 //        document.write('</table>');
 //    }
-    
+
     function Piece(color, name, position){
        this._color = color;
        this._name = name;
        this._position = position;
        }
-    
+
     var whiteQueen = new Piece('white', 'queen', 'D1');
     var whiteKing = new Piece('white', 'king', 'E1');
-    
-    
+
+
     Piece.prototype = {
         getPosition: function(){
             return this._position;
@@ -27,7 +34,7 @@ var assert = require('assert');
             return this._color;
         },
         getName: function(){
-            return this._name; 
+            return this._name;
         },
         getAbbrev: function(){
             return this._name.substring(0,1);
@@ -36,7 +43,7 @@ var assert = require('assert');
             return this.fromCharCode('&#9815');
         }
     };
-    
+
     it('should equal position', function(){
         var Bishop = new Piece('white', 'bishop', 'C1');
         assert.equal(Bishop.getPosition(), 'C1');
@@ -44,6 +51,5 @@ var assert = require('assert');
         assert.equal(Bishop.getAbbrev(), "b");
         assert.equal(Bishop.toHTML(), "b");
     });
-    
-    console.log(whiteQueen);
 
+    console.log(whiteQueen);
