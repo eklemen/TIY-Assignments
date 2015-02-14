3d25745cc1c4bf54051098cea9556f6166c481ac
Evan Klemen <ejklemen@gmail.com>
2015-02-14 00:06:26 -0500

    added mouse events with jquery instead of css hover to popular posts

--- surf-and-paddle/js/main.js
+++ surf-and-paddle/js/main.js
@@ -1,1 +1,8 @@
-
+$(document).ready(
+    $('.pop-box').on('mouseover', function(){
+        $(this).removeClass('overlay');
+}).mouseout(function(){
+        $(this).addClass('overlay');
+    })
+    
+)
