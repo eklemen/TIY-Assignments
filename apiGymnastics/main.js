50a26463f928acef6ce02837c4ae7208fe9ffda1
Evan Klemen <ejklemen@gmail.com>
2015-02-15 16:31:06 -0500

    fount total events

--- apiGymnastics/main.js
+++ apiGymnastics/main.js
@@ -1,1 +1,27 @@
 var events = require('./events.json');
+
+function answer(){
+    console.log(events.length);
+    return {
+        'total': events.length,
+        'PushEvent':{
+            'total':
+        }
+    }
+}
+
+console.log(answer());
+
+//function answer(){
+//  return {
+//    'total': ...,    // How many total events did you fetch?
+//    'PushEvent': {
+//      'total': ...,  // How many total events of type `PushEvent` are there?
+//      'perDay': ...  // On average, how many`PushEvent` entries per day?
+//    },
+//    'other': {
+//      'total': ...,  // How many _other_ events are in the data?
+//      'perDay': ...  // How many per day, on average?
+//    }
+//  };
+//} // END answer
