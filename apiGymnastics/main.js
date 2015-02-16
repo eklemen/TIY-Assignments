4d62fb06701a631a2a327cc177c5ec96ac3d88af
Evan Klemen <ejklemen@gmail.com>
2015-02-15 20:40:18 -0500

    made a function to subtract number of days on PushEvents, refactoring to just all days total

--- apiGymnastics/main.js
+++ apiGymnastics/main.js
@@ -1,23 +1,31 @@
 var events = require('./events.json');
 var _ = require('lodash');
 
+function eventSearch(eventType){
+        return _.filter(events, {'type':eventType});
+    }
+function getDate (eventType){
+    return _.pluck(_.filter(eventSearch(eventType)), 'created_at')
+};
+var pushEventDates = getDate("PushEvent");
+
+function dateDiff(){
+    return _.first(getDate) - _.last(getDate);
+}
+
+
+
 function answer(){
-    console.log(events.length);
     return {
         'total': events.length,
         'PushEvent':{
-            'total': foo("PushEvent").length
+            'total': eventSearch("PushEvent").length,
+            'perDay': _.first(pushEventDates).substring(8, 10) - _.last(pushEventDates).substring(8, 10)
         }
     }
 }
 
-function foo(eventType){
-        return _.filter(events, {'type':eventType});
-    }
-
-
-
-//var pushEvents = _.filter(events, {'type':'PushEvent'})
+console.log(pushEventDates);
 console.log(answer());
 //console.log(answer());
 
