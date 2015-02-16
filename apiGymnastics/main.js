146e5b8d1493258b8370beb0407e24246b4fd1fb
Evan Klemen <ejklemen@gmail.com>
2015-02-15 21:12:32 -0500

    added other total and per day stats, also filtered unique events

--- apiGymnastics/main.js
+++ apiGymnastics/main.js
@@ -4,41 +4,30 @@ var _ = require('lodash');
 function eventSearch(eventType){
         return _.filter(events, {'type':eventType});
     }
-function getDate (eventType){
-    return _.pluck(_.filter(eventSearch(eventType)), 'created_at')
-};
-var pushEventDates = getDate("PushEvent");
+var numOfPushEvents = eventSearch("PushEvent").length;
+var getDates = _.pluck(events, 'created_at');
 
-function dateDiff(){
-    return _.first(getDate) - _.last(getDate);
+function rangeOfEvents(){
+    var firstLastDate = _.first(getDates).substring(8, 10) - _.last(getDates).substring(8, 10) + 1;
+    return firstLastDate;
 }
-
-
+var pushPerDay = numOfPushEvents / rangeOfEvents();
+var otherPerDay = (events.length - numOfPushEvents) / rangeOfEvents();
+var uniqueCount = _.uniq(events, 'type').length;
 
 function answer(){
     return {
         'total': events.length,
+        'unique-events': uniqueCount,
         'PushEvent':{
-            'total': eventSearch("PushEvent").length,
-            'perDay': _.first(pushEventDates).substring(8, 10) - _.last(pushEventDates).substring(8, 10)
+            'total': numOfPushEvents,
+            'perDay': pushPerDay
+        },
+        'other':{
+            'total': events.length - numOfPushEvents,
+            'perDay': otherPerDay
         }
     }
 }
 
-console.log(pushEventDates);
 console.log(answer());
-//console.log(answer());
-
-//function answer(){
-//  return {
-//    'total': ...,    // How many total events did you fetch?
-//    'PushEvent': {
-//      'total': ...,  // How many total events of type `PushEvent` are there?
-//      'perDay': ...  // On average, how many`PushEvent` entries per day?
-//    },
-//    'other': {
-//      'total': ...,  // How many _other_ events are in the data?
-//      'perDay': ...  // How many per day, on average?
-//    }
-//  };
-//} // END answer
