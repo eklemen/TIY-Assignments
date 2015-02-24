abaaeaa4715efc49d74d63c46a330003cf8bad87
Evan Klemen <ejklemen@gmail.com>
2015-02-23 20:50:46 -0500

    2.6

--- pub/js/main.js
+++ pub/js/main.js
@@ -5,6 +5,18 @@
         this.products = gem;
     });
     
+    app.controller("TabController", function(){
+        this.tab = 1;
+        
+        this.setTab = function(selectedTab){
+            this.tab = selectedTab;
+        };
+        
+        this.isSet = function(givenTab){
+        return this.tab === givenTab;
+    }; 
+    });
+    
     var gem = [
         {
             name: 'Azurite',
