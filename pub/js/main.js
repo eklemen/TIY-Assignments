5380f82d344e4e7317a1944057eb90c8cdd3fba0
Evan Klemen <ejklemen@gmail.com>
2015-02-23 21:24:49 -0500

    2.10

--- pub/js/main.js
+++ pub/js/main.js
@@ -17,21 +17,34 @@
     }; 
     });
     
+    app.controller("GalleryController", function(){
+        this.current = 0;
+        this.setCurrent = function(newGallery){
+          this.current = newGallery || 0;  
+        };
+    });
+    
     var gem = [
         {
             name: 'Azurite',
             price: 2.95,
-            images: 'http://lorempixel.com/output/sports-q-c-200-200-6.jpg'
+            images: 'http://lorempixel.com/output/sports-q-c-200-200-6.jpg',
+            description: 'Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sided',
+            shine: '5'
         },
         {
             name: 'Bloodstone',
             price: 5.95,
-            images: 'http://lorempixel.com/output/abstract-q-c-200-200-1.jpg'
+            images: 'http://lorempixel.com/output/abstract-q-c-200-200-1.jpg',
+            description: 'I think this gem was just OK, could honestly use more shine, IMO.',
+            shine: '8'
         },
         {
             name: 'Zircon',
             price: 3.95,
-            images: 'http://lorempixel.com/output/nightlife-q-c-200-200-2.jpg'
+            images: 'http://lorempixel.com/output/nightlife-q-c-200-200-2.jpg',
+            description: 'Even more nice,It has a very high shine and 12 sided',
+            shine: '8'
         }
 
     ];
