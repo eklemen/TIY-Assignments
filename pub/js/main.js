8e3a08ce08dd7d7d43a6b32dc6179a267055a986
Evan Klemen <ejklemen@gmail.com>
2015-02-23 20:38:06 -0500

    2.6

--- pub/js/main.js
+++ pub/js/main.js
@@ -2,13 +2,25 @@
     
     var app = angular.module('gemStore', []);
     app.controller('StoreController', function(){
-        this.product = gem;
+        this.products = gem;
     });
     
     var gem = [
-        { name: 'Azurite', price: 2.95 },
-        { name: 'Bloodstone', price: 5.95 },
-        { name: 'Zircon', price: 3.95 }
+        {
+            name: 'Azurite',
+            price: 2.95,
+            images: 'http://lorempixel.com/output/sports-q-c-200-200-6.jpg'
+        },
+        {
+            name: 'Bloodstone',
+            price: 5.95,
+            images: 'http://lorempixel.com/output/abstract-q-c-200-200-1.jpg'
+        },
+        {
+            name: 'Zircon',
+            price: 3.95,
+            images: 'http://lorempixel.com/output/nightlife-q-c-200-200-2.jpg'
+        }
 
     ];
     
