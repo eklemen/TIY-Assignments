f91720f5cc08e6a5371ac891d3015eb2dcc4044d
Evan Klemen <ejklemen@gmail.com>
2015-03-03 20:39:52 -0500

    about to overwrite this with gulp-webapp lets go

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,50 +1,49 @@
 (function(){
     
     var app = angular.module('gemStore', []);
-    app.controller('StoreController', function(){
-        this.products = gem;
+    app.controller("ReviewController", function(){
+       this.review = {};
+        
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        }
     });
     
-    app.controller("TabController", function(){
-        this.tab = 1;
-        
-        this.setTab = function(selectedTab){
-            this.tab = selectedTab;
-        };
-        
-        this.isSet = function(givenTab){
-        return this.tab === givenTab;
-    }; 
-    });
-    
-    app.controller("GalleryController", function(){
-        this.current = 0;
-        this.setCurrent = function(newGallery){
-          this.current = newGallery || 0;  
-        };
-    });
     
     var gem = [
         {
             name: 'Azurite',
             price: 2.95,
-            images: 'http://lorempixel.com/output/sports-q-c-200-200-6.jpg',
             description: 'Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sided',
-            shine: '5'
+            shine: '5',
+            reviews: [{
+                stars: 5,
+                body: "wow what a gem",
+                author: "joe@example.org"
+        }],
         },
         {
             name: 'Bloodstone',
             price: 5.95,
-            images: 'http://lorempixel.com/output/abstract-q-c-200-200-1.jpg',
             description: 'I think this gem was just OK, could honestly use more shine, IMO.',
-            shine: '8'
+            shine: '8',
+            reviews: [{
+                stars: 3,
+                body: "Nice gem",
+                author: "joe@example.org"
+        }],
         },
         {
             name: 'Zircon',
             price: 3.95,
-            images: 'http://lorempixel.com/output/nightlife-q-c-200-200-2.jpg',
             description: 'Even more nice,It has a very high shine and 12 sided',
-            shine: '8'
+            shine: '8',
+            reviews: [{
+                stars: 5,
+                body: "Really great product",
+                author: "joe@example.com"
+        }],
         }
 
     ];
