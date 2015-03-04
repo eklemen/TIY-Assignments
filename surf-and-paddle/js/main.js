9523a7d16bd1b43f0057d8a932b081f9a0709675
Evan Klemen <ejklemen@gmail.com>
2015-03-03 20:42:20 -0500

    DELETED ALL THE THINGS

--- surf-and-paddle/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(
-    $('.pop-box').on('mouseover', function(){
-        $(this).removeClass('overlay');
-}).mouseout(function(){
-        $(this).addClass('overlay');
-    })
-    
-)
